cadcb706ea6b1ed161f57ff0b930dc1c104786e2
Carlos Rodriguez <carlos.a.rodriguez100@gmail.com>
2018-10-11 10:59:12 -0400

    added some styling with css

--- script/index.js
+++ script/index.js
@@ -3,10 +3,15 @@ const triggerELement = document.querySelector(`[data-trigger]`);
 const outputElement = document.querySelector('[data-output]')
 
 
-triggerELement.addEventListener('click', function() {
-    console.log("Hello");
-    outputElement.textContent = 'lalallalal'
-    
+triggerELement.addEventListener('click', function() { 
+    outputElement.textContent = fortunes.pop();  
 });
 
+const fortunes = [
+    "Meh",
+    "Ignore previous fortunes",
+    "A new pair of shoes will do you a world of good",
+    "Some fortune cookies contain no fortune",
+    "Today is propably a huge improvement over yesterday"
+]
 
